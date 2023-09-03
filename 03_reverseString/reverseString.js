fdade7bdb19a022ba48eded7722a52ddf0543b26
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-09-02 23:41:57 -0400

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
-};
+const reverseString = function (str) {
+    return str.split("").reverse().join("");
+  };
+// spliting the string reversing it then joining it
 
 // Do not edit below this line
 module.exports = reverseString;
