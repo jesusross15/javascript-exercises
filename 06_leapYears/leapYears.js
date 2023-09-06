d36d3efb2973a21672da1b92aaecf6d54a7017f5
Jesus Ross <91804598+jesusross15@users.noreply.github.com>
2023-09-06 16:11:45 -0400

    06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true; // It's a leap year
+      } else {
+        return false; // It's not a leap year
+      }
 };
+// Had trouble with this one. My logic was correct. But bc I was used to python I forgot Booleans aren't numeric types.
 
+// They in fact are truthy or falsely. I did look at the solution to help me with the syntax and tried to incoorporate it in my own way
 // Do not edit below this line
 module.exports = leapYears;
